8af61eac16ff77b4b971f84574d860c3a6fae858
Ivan Tag <vanyatag95@gmail.com>
2026-04-07 12:06:07 +0500

    feat: update SVG icons for dashboard navigation with new designs

--- frontend/src/components/NavIcons.tsx
+++ frontend/src/components/NavIcons.tsx
@@ -1,24 +1,22 @@
 // Futuristic SVG icons for dashboard navigation
+
 export const DashboardIcon = (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3" y="3" width="18" height="18" rx="6" fill="#111"/>
-    <circle cx="12" cy="12" r="6" stroke="#14F195" strokeWidth="2.5" fill="none"/>
-    <circle cx="12" cy="12" r="2" fill="#14F195"/>
+  <svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#a)"><path fillRule="evenodd" clipRule="evenodd" d="M10.27 14.1a6.5 6.5 0 0 0 3.67-3.45q-1.24.21-2.7.34-.31 1.83-.97 3.1M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.48-1.52a7 7 0 0 1-.96 0H7.5a4 4 0 0 1-.84-1.32q-.38-.89-.63-2.08a40 40 0 0 0 3.92 0q-.25 1.2-.63 2.08a4 4 0 0 1-.84 1.31zm2.94-4.76q1.66-.15 2.95-.43a7 7 0 0 0 0-2.58q-1.3-.27-2.95-.43a18 18 0 0 1 0 3.44m-1.27-3.54a17 17 0 0 1 0 3.64 39 39 0 0 1-4.3 0 17 17 0 0 1 0-3.64 39 39 0 0 1 4.3 0m1.1-1.17q1.45.13 2.69.34a6.5 6.5 0 0 0-3.67-3.44q.65 1.26.98 3.1M8.48 1.5l.01.02q.41.37.84 1.31.38.89.63 2.08a40 40 0 0 0-3.92 0q.25-1.2.63-2.08a4 4 0 0 1 .85-1.32 7 7 0 0 1 .96 0m-2.75.4a6.5 6.5 0 0 0-3.67 3.44 29 29 0 0 1 2.7-.34q.31-1.83.97-3.1M4.58 6.28q-1.66.16-2.95.43a7 7 0 0 0 0 2.58q1.3.27 2.95.43a18 18 0 0 1 0-3.44m.17 4.71q-1.45-.12-2.69-.34a6.5 6.5 0 0 0 3.67 3.44q-.65-1.27-.98-3.1" fill="#14F195"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h16v16H0z"/></clipPath></defs>
   </svg>
 );
 
 export const EconomicsIcon = (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3" y="3" width="18" height="18" rx="6" fill="#111"/>
-    <path d="M7 17L11 13L14 16L17 10" stroke="#14F195" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-    <circle cx="17" cy="10" r="1.5" fill="#14F195"/>
+    <rect x="2" y="2" width="20" height="20" rx="6" fill="#111"/>
+    <path d="M6 18L12 12L16 16L20 7" stroke="#14F195" strokeWidth="3.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <circle cx="20" cy="7" r="2.2" fill="#14F195"/>
   </svg>
 );
 
+
 export const SimulatorIcon = (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="3" y="3" width="18" height="18" rx="6" fill="#111"/>
-    <rect x="8" y="8" width="8" height="8" rx="2" stroke="#14F195" strokeWidth="2.5" fill="none"/>
-    <circle cx="12" cy="12" r="1.5" fill="#14F195"/>
+  <svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M1.5 2.5h13v10a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1zM0 1h16v11.5a2.5 2.5 0 0 1-2.5 2.5h-11A2.5 2.5 0 0 1 0 12.5zm3.75 4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5M7 4.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0m1.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5" fill="#14F195"/>
   </svg>
 );
